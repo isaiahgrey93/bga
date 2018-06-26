3f6285d4690e8d688a1ef95e58a3b726cb5e59b1
Isaiah Grey <isaiahgrey@gmail.com>
2018-06-25 17:20:28 -0700

    add row component/story

--- .storybook/stories/row/Row.js
+++ .storybook/stories/row/Row.js
@@ -3,11 +3,23 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import withPropsCombinations from 'react-storybook-addon-props-combinations';
+import { text } from '@storybook/addon-knobs/react';
 
 import { decorateAddons } from 'story-decorators';
 
 import { Row } from 'components';
 
 decorateAddons(storiesOf('Row', module))
-  .addWithJSX('Component', () => <Row />)
-  .add('Prop Combinations', withPropsCombinations(Row, {}));
+  .addWithJSX('Component', () => (
+    <Row
+      left={text('Left Text/Node(s)', 'TextLeft')}
+      right={text('Right Text/Node(s)', 'TextRight')}
+    />
+  ))
+  .add(
+    'Prop Combinations',
+    withPropsCombinations(Row, {
+      right: ['TextRight'],
+      left: ['TextLeft'],
+    })
+  );
--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -1,3 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default () => <center>Not Implemented</center>;
+import { Row as StyledRow, } from './styles';
+
+const Row = ({ right, left, ...props }) => (
+  <StyledRow {...props}>
+    <div>{left}</div>
+    <div>{right}</div>
+  </StyledRow>
+);
+
+Row.propTypes = {
+  right: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
+  left: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
+};
+
+Row.defaultProps = {
+  right: undefined,
+  left: undefined,
+};
+
+export default Row;
